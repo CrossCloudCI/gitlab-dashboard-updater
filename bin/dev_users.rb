96616042768580aad15db16fc6a04a8c45e22f96
DenverWilliams <denver@debian.nz>
2017-12-06 09:57:17 +1300

    Add the STAGING_EMAIL

--- bin/dev_users.rb
+++ bin/dev_users.rb
@@ -14,6 +14,8 @@ module CrossCloudCI
         dashboard_api_host_port=ENV["DENVER_DASHBOARD_API_HOST_PORT"] if ENV["DENVER_DASHBOARD_API_HOST_PORT"]
       when ENV["ANDRE_EMAIL"]
         dashboard_api_host_port=ENV["ANDRE_DASHBOARD_API_HOST_PORT"] if ENV["ANDRE_DASHBOARD_API_HOST_PORT"]
+      when ENC["STAGING_EMAIL"]
+        dashboard_api_host_port=ENV["STAGING_DASHBOARD_API_HOST_PORT"] if ENV["STAGING_DASHBOARD_API_HOST_PORT"]
       end
     end
   end
