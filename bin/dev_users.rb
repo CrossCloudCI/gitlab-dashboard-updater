d3a30b8e2dab2a7ad8aa57041cd9263c968495b6
Taylor Carpenter <taylor@vulk.coop>
2017-12-04 21:34:18 -0500

    adding dev users script temp.  updated logic to only require if it's there

---
+++ bin/dev_users.rb
@@ -0,0 +1,20 @@
+module CrossCloudCI
+  module DashboardUtils
+    def load_users_api_host_port
+      case ENV["GITLAB_USER_EMAIL"]
+      when ENV["LUCINA_EMAIL"]
+        dashboard_api_host_port=ENV["LUCINA_DASHBOARD_API_HOST_PORT"] if ENV["LUCINA_DASHBOARD_API_HOST_PORT"]
+      when ENV["WAVELL_EMAIL"]
+        dashboard_api_host_port=ENV["WAVELL_DASHBOARD_API_HOST_PORT"] if ENV["WAVELL_DASHBOARD_API_HOST_PORT"]
+      when ENV["JOSH_EMAIL"]
+        dashboard_api_host_port=ENV["JOSH_DASHBOARD_API_HOST_PORT"] if ENV["JOSH_DASHBOARD_API_HOST_PORT"]
+      when ENV["TAYLOR_EMAIL"]
+        dashboard_api_host_port=ENV["TAYLOR_DASHBOARD_API_HOST_PORT"] if ENV["TAYLOR_DASHBOARD_API_HOST_PORT"]
+      when ENV["DENVER_EMAIL"]
+        dashboard_api_host_port=ENV["DENVER_DASHBOARD_API_HOST_PORT"] if ENV["DENVER_DASHBOARD_API_HOST_PORT"]
+      when ENV["ANDRE_EMAIL"]
+        dashboard_api_host_port=ENV["ANDRE_DASHBOARD_API_HOST_PORT"] if ENV["ANDRE_DASHBOARD_API_HOST_PORT"]
+      end
+    end
+  end
+end
