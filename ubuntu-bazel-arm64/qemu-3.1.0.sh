53dbbd2fcab2193539797dd06d2cd13b68ef2f25
denverwilliams <denver@debian.nz>
2019-05-07 09:41:55 -0400

    Install missing deps & don't use sudo

--- ubuntu-bazel-arm64/qemu-3.1.0.sh
+++ ubuntu-bazel-arm64/qemu-3.1.0.sh
@@ -6,7 +6,7 @@ tar -xf qemu-3.1.0.tar.bz2
 cd qemu-3.1.0
 ./configure --target-list=arm-linux-user,aarch64-linux-user --prefix=/opt/qemu --static
 make
-sudo make install
+make install
 cd /opt/qemu/bin
-sudo ln -s qemu-arm qemu-arm-static
-sudo ln -s qemu-aarch64 qemu-aarch64-static
+ln -s qemu-arm qemu-arm-static
+ln -s qemu-aarch64 qemu-aarch64-static
