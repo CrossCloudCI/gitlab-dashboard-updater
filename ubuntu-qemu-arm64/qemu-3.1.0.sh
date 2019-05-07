c341d5142cb88aef5792a88c650cdb3aeb2c7c3a
denverwilliams <denver@debian.nz>
2019-05-07 12:33:01 -0400

    Move qemu to the correct dir

--- ubuntu-qemu-arm64/qemu-3.1.0.sh
+++ ubuntu-qemu-arm64/qemu-3.1.0.sh
@@ -7,6 +7,3 @@ cd qemu-3.1.0
 ./configure --target-list=arm-linux-user,aarch64-linux-user --prefix=/opt/qemu --static
 make
 make install
-cd /opt/qemu/bin
-ln -s qemu-arm qemu-arm-static
-ln -s qemu-aarch64 qemu-aarch64-static
