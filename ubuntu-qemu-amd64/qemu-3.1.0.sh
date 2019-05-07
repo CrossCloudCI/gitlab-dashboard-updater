39e04269adb9df67e307e8b5486b92aee727d63e
denverwilliams <denver@debian.nz>
2019-05-07 16:35:12 -0400

    Update configuration for x86_64

--- ubuntu-qemu-amd64/qemu-3.1.0.sh
+++ ubuntu-qemu-amd64/qemu-3.1.0.sh
@@ -4,6 +4,6 @@ rm -rf qemu/
 wget http://wiki.qemu-project.org/download/qemu-3.1.0.tar.bz2
 tar -xf qemu-3.1.0.tar.bz2
 cd qemu-3.1.0
-./configure --target-list=arm-linux-user,aarch64-linux-user --prefix=/opt/qemu --static
+./configure --target-list=x86_64-linux-user --prefix=/opt/qemu --static
 make
 make install
