76bda4d6a93361746cbce34f8918a535b2b0fd20
denverwilliams <denver@debian.nz>
2019-05-08 15:22:53 -0400

    Revert back to old arm64 bazel build

--- ubuntu-qemu-amd64/qemu-3.1.0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /opt
-rm -rf qemu/
-wget http://wiki.qemu-project.org/download/qemu-3.1.0.tar.bz2
-tar -xf qemu-3.1.0.tar.bz2
-cd qemu-3.1.0
-./configure --target-list=x86_64-linux-user --prefix=/opt/qemu --static
-make
-make install
